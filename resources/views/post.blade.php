667e311061110d99bf90f203b97b009a4f89a5ce
nemanjakovi <nemanjakovi90@gmail.com>
2022-09-14 09:21:08 +0200

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,24 @@
+<x-layout>
+
+
+
+<article>
+
+    <p>
+        <h1> {{ $post->title }} </h1>
+        </p>
+
+        <div>
+            {!! $post->body !!} 
+        </div>
+
+</article>
+ <a href="/">Go back</a>
+</x-layout>
+
+
+    
+
+    
+
+    
