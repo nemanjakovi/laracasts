15de5f63f7659b592fc7360d3a63f6412aaadb59
nemanjakovi <nemanjakovi90@gmail.com>
2022-10-08 12:06:52 +0200

    Build the Login page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+// use Dotenv\Exception\ValidationException;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+
+    public function store()
+    {
+        $attributes =  request()->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+
+            return redirect('/')->with('success', 'Welcome back');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+
+        return back()
+            ->withInput()
+            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+    }
+
+
+
     public function destroy()
     {
         auth()->logout();
